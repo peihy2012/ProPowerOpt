2158fb6a909b7843c8e9ef6368b37f8332a24cf8
peihy2012 <peihy2012@gmail.com>
2016-05-25 20:39:23 +0800

    create new folder and function. put some data and functions into GridData folder.

---
+++ MODA/GridData/RPOF1.m
@@ -0,0 +1,25 @@
+function [ o ] = RPOF1( X, pf, Snew )
+%RPOF1 Summary of this function goes here
+%   Detailed explanation goes here
+
+nodeloc = [5 13 20 23 31];
+Qc = zeros(pf.nb,1);
+Qc(nodeloc) = round(X)*0.05*1j;
+%% power flow
+spVm = [];
+spVa = [];
+spLoss = [];
+
+pf1 = pf;
+pf1.Sbus = pf1.busdata(:,5) + j*pf1.busdata(:,6) - Qc;
+pf1.powerflow();
+pf1.powerloss();
+spVm = pf1.Vm; 
+spVa = pf1.Va; 
+spLoss = sum(pf1.loss);
+
+spVm = spVm/pf.baseKV;
+o(1) = sum((spVm-1).^2);
+o(2) = spLoss;
+end
+
---
+++ MODA/MODAopt.m
@@ -0,0 +1,44 @@
+
+
+function MODAopt(  )
+%MODAOPT Summary of this function goes here
+%   Detailed explanation goes here
+clc;
+clear;
+close all;
+%% power flow initialize
+addpath('.\GridData');
+% global pf Snew
+pf = PowerFlowRadia(busdata33);
+simpleNum = 100000;
+%% include Pwind and pwrSmp
+load('Pwind_0512.mat');
+load('pwrSmp_0512.mat');
+windRate = 0.9;
+windLoc = 24;
+phi = acos(windRate);
+Swind = Pwind.*(1+i*tan(phi));
+pwrSmp(windLoc,:) = pwrSmp(windLoc,:) - Swind/4;
+Snew = pwrSmp;
+%% initialize
+pf.makeYbus();
+pf.makeSbus();
+pf.initPowerflow();
+%% MODA optimization process
+ObjectiveFunction=@RPOF1;
+mo = MODA(@RPOF1);
+mo.init();
+for iter = 1:100
+    for it=1:mo.N 
+        mo.Particles_F(it,:) = ObjectiveFunction(mo.X(:,it)', pf, Snew);
+    end
+    mo.getfront();
+    mo.operation(iter);
+    mo.plot(iter);
+    display(['Iteration = ', num2str(iter), ' , ', num2str(mo.ArchiveSize), ' non-dominated solutions']);
+    pause(0.05);
+end
+
+
+end
+
