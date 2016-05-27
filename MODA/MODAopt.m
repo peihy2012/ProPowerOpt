ad9c8ad3ea5a410571de722ea3129421165585d1
peihy2012 <peihy2012@gmail.com>
2016-05-27 11:17:50 +0800

    Add the third objective and show the compute time. Use parfor to parallel compute.

--- MODA/MODAopt.m
+++ MODA/MODAopt.m
@@ -19,28 +19,37 @@ sampleNum = 100000;
 % Swind = Pwind.*(1+i*tan(phi));
 pwrSmp = powersample(sampleNum,data33.busdata);
 Swind = windsample(sampleNum,1);
-windLoc = 24;
+Ssolar = solarsample(sampleNum,1);
+windLoc = [24];
+solarLoc = [17];
 pwrSmp(windLoc,:) = pwrSmp(windLoc,:) - Swind/4;
+pwrSmp(solarLoc,:) = pwrSmp(solarLoc,:) - Ssolar/4;
 Snew = pwrSmp;
 %% initialize
 pf.makeYbus();
 pf.makeSbus();
 pf.initPowerflow();
 %% MODA optimization process
-ObjectiveFunction=@RPOF1;
-mo = MODA(@RPOF1);
+ObjectiveFunction=@RPOF;
+mo = MODA();
 mo.init();
+t0 = clock;
 for iter = 1:100
-    for it=1:mo.N 
-        mo.Particles_F(it,:) = ObjectiveFunction(mo.X(:,it)', pf, Snew);
+    t1 = clock;
+    parfor it=1:mo.N 
+        % mo.Particles_F(it,:) = ObjectiveFunction(mo.X(:,it)', pf, Snew);
+        P_F(it,:) = ObjectiveFunction(mo.X(:,it)', pf, Snew);
     end
+    mo.Particles_F = P_F;
     mo.getfront();
     mo.operation(iter);
     mo.plot(iter);
-    display(['Iteration = ', num2str(iter), ' , ', num2str(mo.ArchiveSize), ' non-dominated solutions']);
+    t2 = etime(clock, t1);
+    display(['Iteration = ', num2str(iter), ' , ', num2str(mo.ArchiveSize), ' non-dominated solutions , time = ', num2str(t2) , ' .']);
     pause(0.05);
 end
-
-
+t2 = etime(clock,t0);
+display([ 'time = ', num2str(t2) , ' .']);
+save moda_result mo
 end
 
